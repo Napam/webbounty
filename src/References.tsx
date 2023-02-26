b4fe66bcd483039233c3153a9a86e73d5705407e
Naphat Amundsen <naphat@live.no>
2023-02-26 12:29:58 +0100

    more on references

--- src/References.tsx
+++ src/References.tsx
@@ -2,29 +2,50 @@ import { Accordion, AccordionTab } from 'primereact/accordion';
 import styled from 'styled-components/macro'
 
 export default function References() {
-  let options = {
+  let options: Intl.DateTimeFormatOptions = {
     weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   };
 
+  const header = () => (
+    <HeaderContainer>
+      <span className="reference-date">
+        <i className="pi pi-calendar"></i>
+        <span>
+          {Intl.DateTimeFormat('en', options).format(new Date(Date.UTC(Math.round(Math.random() * 25) + 2000, Math.round(Math.random() * 11), 10)))}
+        </span>
+      </span>
+
+      <span>
+        <i className="pi pi-wallet"></i>
+        <span className="reference-balance">
+          {new Intl.NumberFormat('en', {maximumFractionDigits: 2, style: "decimal", notation: "standard"}).format((Math.random() * 35))}
+        </span>
+      </span>
+    </HeaderContainer>
+  )
+
+  const Content = () => (
+    <>
+      Test
+    </>
+  )
+
   return (
     <>
       <ReferencesAccordion multiple activeIndex={[0]}>
-        <AccordionTab header={Intl.DateTimeFormat('en', options).format(new Date(Date.UTC(2020, 11, 10)))}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <AccordionTab header={header()}>
+          <Content/>
         </AccordionTab>
-        <AccordionTab header={Intl.DateTimeFormat('en', options).format(new Date(Date.UTC(2022, 2, 10)))}>
+        <AccordionTab header={header()}>
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
           quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
           sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
           Consectetur, adipisci velit, sed quia non numquam eius modi.
         </AccordionTab>
-        <AccordionTab header={Intl.DateTimeFormat('en', options).format(new Date(Date.UTC(2023, 10, 10)))}>
+        <AccordionTab header={header()}>
           At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
           quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt
           mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
@@ -38,5 +59,27 @@ export default function References() {
 const ReferencesAccordion = styled(Accordion)`
   .p-accordion-tab {
     margin-bottom: 14px;
+  }
+
+  .p-accordion-header-text {
+    width: 100%;
+  }
+`
+
+const HeaderContainer = styled.span`
+  display: flex;
+  width: 100%;
+  max-width: 22em;
+  & > span > i {
+    margin-right: 8px;
+  }
+
+  & > .reference-date {
+    flex-grow: 1;
+  }
+
+  & > span > .reference-balance {
+    display: inline-block;
+    width: 3em;
   }
 `
